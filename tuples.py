39c1c07e0303d240f9f21da57a84016d31dd1511
shokhrukhbek <shohruhinomjonov691@gmail.com>
2026-05-25 23:53:06 +0900

    feat: unpacking argument via tuple

--- tuples.py
+++ tuples.py
@@ -25,3 +25,32 @@ tuple_obj = ("MIT", 100, True, None)
 
 print(animals[0])
 # animals[0] = "bird"
+
+# try avoid these
+people = "Andrew", "John"
+animals = "dog",
+
+
+print("===== Unpacking arguments =====")
+groups = ["MIT", "FLEXY", "DEVEX", "MG"]
+(x, y, *z) = groups
+print(f"the x: {x} and y: {y}")
+print("z:", z)  # list
+
+
+# *args > tuple
+def calculate(*args):
+    print("*args >", args)
+    total = 1
+    for x in args:
+        total *= x
+    print(f"the total value: {total}")
+    return total
+
+
+# CALL
+calculate(1, 7, 2, 3)
+print("-----")
+calculate(0, 2, 300)
+print("-----")
+calculate(5, 7)

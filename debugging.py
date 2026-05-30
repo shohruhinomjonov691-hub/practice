30b25b69b5bd97a28b6e601e98d8abb5bf99c623
shokhrukhbek <shohruhinomjonov691@gmail.com>
2026-05-30 16:03:12 +0900

    feat: debugging process

--- debugging.py
+++ debugging.py
@@ -12,11 +12,11 @@ print("===== Python Packages & Core Package =====")
 
 
 # Core package
-# t = turtle.Turtle()
-# t.shape("turtle")
-# t.speed(2)
-# t.circle(150)
-# turtle.done()
+t = turtle.Turtle()
+t.shape("turtle")
+t.speed(2)
+t.circle(150)
+turtle.done()
 
 
 my_file = open("material/message.txt", "r")
@@ -42,3 +42,18 @@ with Image.open("material/logo.png") as img_obj:
     resized_img = img_obj.resize((200, 200))
     resized_img.show()
     resized_img.save("material/sample.png")
+
+
+print("===== Debugging =====")
+
+
+def get_summary(*args):  # DEFINE
+    total_amount = 0
+    for a in args:
+        total_amount += a
+        return total_amount  # find the bug via debugging
+
+
+test = 100
+result = get_summary(1, 2, 3, 4, 5)  # CALL
+print("result:", result)

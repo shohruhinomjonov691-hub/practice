52b2a4c22213573916179d1ed349c8a4f976a815
shokhrukhbek <shohruhinomjonov691@gmail.com>
2026-05-30 14:58:31 +0900

    feat: Core Packages study

---
+++ debugging.py
@@ -0,0 +1,34 @@
+''' Packages & Debugging
+    (1) Python Packages & Core Package
+    (2) Package Manager & External Package
+    (3) Debugging
+'''
+
+import turtle
+print("===== Python Packages & Core Package =====")
+''' Python Packages/Modules: Core, File and External '''
+# Core Packages > https://docs.python.org/3/library
+
+
+# Core package
+# t = turtle.Turtle()
+# t.shape("turtle")
+# t.speed(2)
+# t.circle(150)
+# turtle.done()
+
+
+my_file = open("material/message.txt", "r")
+try:
+    content = my_file.read()
+    print("content:", content)
+finally:
+    my_file.close()
+
+# with
+with open("material/message.txt", "r") as your_file:
+    your_content = your_file.read()
+    print("your_content:", your_content)
+
+print("DONE")
+ 

b5abc06499a96c9861020dec675cc97e91f03412
shokhrukhbek <shohruhinomjonov691@gmail.com>
2026-05-30 14:35:41 +0900

    feat: set & dictionary comprehension

--- comprehension.py
+++ comprehension.py
@@ -21,7 +21,7 @@ print(numbers is list_numbers)
 print(id(numbers), id(list_numbers))
 
 print("------")
-people = [("Robert", 20), ("Steve", 19), ("Joseph", 25)]
+people = [("Robert", 21), ("Steve", 19), ("Tony", 25)]
 list_people = [person[0] for person in people]  # b version
 print("list_people:", list_people)
 
@@ -35,3 +35,13 @@ cars = [
 ]
 list_cars = [car[0] for car in cars if car[1] > 80]  # c version
 print("list_cars:", list_cars)
+
+print("===== set and dictionary comprehension =====")
+numbs = [1, 5, 4, 20, 4, 5, 1, 4]
+set_numbs = {*numbs}
+print("set_numbs:", set_numbs)
+
+dict_people = {person[0]: person[1] for person in people}  # b version
+print("dict_people:", dict_people)
+
+# (<expression> for item in iterable) > generic
